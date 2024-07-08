7efd52fcd7050f2c44e3eb731ef330ec9c22bd17
Ege Sualp <egesualp@gmail.com>
2024-07-08 02:51:53 +0200

    DL Labs and inference practises

--- ss24/practice_functions.R
+++ ss24/practice_functions.R
@@ -6,3 +6,6 @@ result_list <- lapply(3:33, function(x) x^2)
 result_list
 
 result_list <- lapply(1:10, function(x) paste('id: ', x))
+
+exp(1.63197) + 1.96*exp(1.63197)*0.05895
+sqrt(exp(2 * 1.63197) / exp(1.63197))
